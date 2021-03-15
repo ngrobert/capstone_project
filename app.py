d1a272273bd31263d38581f5a58aed9ac871d673
Robert Ng <robert.ng@xandr.com>
2021-03-15 14:33:57 -0400

    Update app run

--- app.py
+++ app.py
@@ -264,4 +264,5 @@ def create_app(test_config=None):
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    # app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0')
