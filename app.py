2cba8a1de6b092d7a14663bdd65c3d9e02e88a49
Robert Ng <robert.ng@xandr.com>
2021-03-17 15:09:32 -0400

    Update Procfile

--- app.py
+++ app.py
@@ -263,5 +263,6 @@ def create_app(test_config=None):
 
 app = create_app()
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
